e11b6ed69c2524fd52d8f0d867964a3d640e0fc8
Mubasser Kamal <mkamal@netspi.com>
2019-06-24 14:46:17 -0500

    Update markmycode.go

--- markmycode.go
+++ markmycode.go
@@ -1,5 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	fileName := flag.String("file", "", "File to be parsed (required)")
+	progLang := flag.String("lang", "", "Programming language of the source file (required)")
+	style := flag.String("css", "", "stylesheet (optional)")
+
+	if len(os.Args) < 3 {
+		fmt.Println("Number of arguments is invalid. Usage is -file <filename> -lang <language>")
+		os.Exit(1)
+	}
+	flag.Parse()
+	if *fileName == "" {
+		fmt.Println("A valid file name is required.")
+		os.Exit(1)
+	}
+	data, err := ioutil.ReadFile(*fileName)
+	check(err)
+	fmt.Println(progLang,style)
+	fmt.Print(string(data))
+}
