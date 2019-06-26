d19cb37ae5b6f27cd8a38d16530ca90f835d7f47
Mubasser Kamal <mkamal@netspi.com>
2019-06-26 10:14:12 -0500

    Update markmycode.go

--- markmycode.go
+++ markmycode.go
@@ -17,18 +17,26 @@ func check(e error) {
 
 func main() {
 	fileName := flag.String("file", "", "File to be parsed (required)")
-	// progLang := flag.String("lang", "", "Programming language of the source file (required)")
-	// style := flag.String("css", "", "stylesheet (optional)")
+	progLang := flag.String("lang", "", "Programming language of the source file (required)")
+	style := flag.String("css", "", "stylesheet (optional)")
 
 	if len(os.Args) < 3 {
 		fmt.Println("Number of arguments is invalid. Usage is -file <filename> -lang <language>")
 		os.Exit(1)
 	}
+
 	flag.Parse()
+
 	if *fileName == "" {
 		fmt.Println("A valid file name is required.")
 		os.Exit(1)
 	}
+
+	if *progLang == ""{
+		fmt.Println("A valid programming language is required.")
+		os.Exit(1)
+	}
+
 	data,err := os.Open(*fileName)
 	check(err)
 	defer data.Close()
